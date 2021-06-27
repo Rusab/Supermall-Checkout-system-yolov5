fceda2029a634693f6e629c9854c6452eb31aa3c
Rusab <rusabsarmun@gmail.com>
2021-06-27 17:26:36 +0600

    implements new ui

--- custom.py
+++ custom.py
@@ -7,7 +7,7 @@ Usage:
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QGraphicsColorizeEffect
-from PyQt5.QtGui import QPixmap, QColor
+from PyQt5.QtGui import QPixmap, QColor, QPalette
 import cv2
 import sys
 
@@ -235,82 +235,264 @@ def run(weights='runs/train/exp11cat16_augmented/weights/best.pt',  # model.pt p
 
 
 
-class Ui_Dialog(object):
-    def setupUi(self, Dialog):
-        Dialog.setObjectName("Automated Checkout System")
-        Dialog.resize(1480, 720)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(1144, 851)
+        
+        palette = QPalette()
+        palette.setColor(QPalette.Text, QtCore.Qt.white)
         
         
-        myFont=QtGui.QFont()
+        myFont=QtGui.QFont('Poppins')
         myFont.setBold(True)
-        myFont.setPointSize(15)
+        myFont.setPointSize(16)
         
+        titleFont=QtGui.QFont('Poppins')
+        titleFont.setBold(True)
+        titleFont.setPointSize(30)
         
-        self.labelCam = QtWidgets.QLabel(Dialog)
-        self.labelCam.setText("Camera Feed")
-        self.labelCam.setFont(myFont)
-        self.labelCam.setGeometry(70, 20, 125, 20)
-        self.labelCam.adjustSize()
-        
-        #self.labelCam.setAlignment(Qt.AlignCenter)
-        self.label = QtWidgets.QLabel(Dialog)
-        self.label.setGeometry(QtCore.QRect(70, 50, 640, 480))
-        self.label.setLineWidth(3)
-        self.label.setText("")
-        self.label.setPixmap(QPixmap("ui_elements/connectingcam.jpg"))
-        self.label.setScaledContents(True)
-        self.label.setObjectName("label")
-        
-        self.listWidget = QtWidgets.QListWidget(Dialog)
-        self.listWidget.setGeometry(QtCore.QRect(720, 50, 300, 480))
-        
-        self.listPrice = QtWidgets.QListWidget(Dialog)
-        self.listPrice.setGeometry(QtCore.QRect(1010, 50, 300, 480))
-        
-        self.button_flag = False
-        self.lock_pointer = 1
+        self.button_flag = 0
         self.lock_flag = 0
+        self.lock_pointer = 1
         self.reset_flag = 0
         
-        self.labelList = QtWidgets.QLabel(Dialog)
-        self.labelList.setText("Product List")
-        self.labelList.setFont(myFont)
-        self.labelList.setGeometry(720, 20, 120, 20)
+        MainWindow.setStyleSheet("""
+        QMainWindow {
+            background-color: rgb(37, 39, 40);
+        }
+        
+        QMenuBar{
+            background-color: rgb(30, 32, 33);
+        }
+        
+        QMenuBar::item::selected {
+            background-color: #000000;
+        }
+        
+        QMenu {
+            background-color: #3b3e3f;
+        }
+        
+        QMenu::item::selected {
+            background-color: #000000;
+        }
+                                 
+                                 """)
+                                 
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.camFeed = QtWidgets.QLabel(self.centralwidget)
+        self.camFeed.setGeometry(QtCore.QRect(50, 170, 640, 480))
+        self.camFeed.setText("")
+        self.camFeed.setPixmap(QtGui.QPixmap("ui_elements/connectingcam.jpg"))
+        self.camFeed.setObjectName("camFeed")
+        
+        self.labelCam = QtWidgets.QLabel(self.centralwidget)
+        self.labelCam.setGeometry(QtCore.QRect(50, 125, 151, 41))
+        self.labelCam.setObjectName("labelCam")
+        self.labelCam.setFont(myFont)
+        self.labelCam.setPalette(palette)
+        self.labelCam.setText("Camera Feed")
+        self.labelCam.setStyleSheet("color:#ffffff")
+        
+        
+        
+        self.listWidget = QtWidgets.QListWidget(self.centralwidget)
+        self.listWidget.setGeometry(QtCore.QRect(720, 170, 241, 481))
+        self.listWidget.setStyleSheet("color: #ffffff; background-color: rgb(59, 62, 63);")
         self.listWidget.setObjectName("listWidget")
         
-        self.pushButton = QtWidgets.QPushButton(Dialog)
-        self.pushButton.setGeometry(QtCore.QRect(110, 540, 240, 80))       
-        self.pushButton.setObjectName("pushButton")
-        self.pushButton.clicked.connect(self.button_clicked)
+        self.listPrice = QtWidgets.QListWidget(self.centralwidget)
+        self.listPrice.setGeometry(QtCore.QRect(970, 170, 121, 481))
+        self.listPrice.setStyleSheet("background-color: rgb(59, 62, 63);")
+        self.listPrice.setObjectName("listPrice")
         
-        self.resetButton = QtWidgets.QPushButton(Dialog)
-        self.resetButton.setGeometry(QtCore.QRect(270, 640, 240, 80))       
-        self.resetButton.setObjectName("resetButton")
-        self.resetButton.clicked.connect(self.reset_list)
+        self.listLabel = QtWidgets.QLabel(self.centralwidget)
+        self.listLabel.setGeometry(QtCore.QRect(720, 125, 151, 41))
+        self.listLabel.setObjectName("listLabel")
+        self.listLabel.setText("Product List")
+        self.listLabel.setFont(myFont)
+        self.listLabel.setStyleSheet("color:#ffffff")
         
-        self.lockButton = QtWidgets.QPushButton(Dialog)
-        self.lockButton.setGeometry(QtCore.QRect(430, 540, 240, 80))  
+        self.horizontalLayoutWidget_2 = QtWidgets.QWidget(self.centralwidget)
+        self.horizontalLayoutWidget_2.setGeometry(QtCore.QRect(220, 40, 651, 71))
+        self.horizontalLayoutWidget_2.setObjectName("horizontalLayoutWidget_2")
+        self.Titlelayout = QtWidgets.QHBoxLayout(self.horizontalLayoutWidget_2)
+        self.Titlelayout.setContentsMargins(0, 0, 0, 0)
+        self.Titlelayout.setObjectName("Titlelayout")
+        
+        self.label_4 = QtWidgets.QLabel(self.centralwidget)
+        self.label_4.setGeometry(QtCore.QRect(220, 20, 631, 111))
+        self.label_4.setText("Automated Billing System")
+        self.label_4.setObjectName("label_4")
+        self.label_4.setFont(titleFont)
+        self.label_4.setStyleSheet("color:#5aff6d")
+        self.Titlelayout.addWidget(self.label_4)
+
+        self.versionLabel = QtWidgets.QLabel(self.centralwidget)
+        self.versionLabel.setGeometry(QtCore.QRect(760, 60, 151, 41))
+        self.versionLabel.setObjectName("versionLabel")
+        self.versionLabel.setText("v.1.0")
+        self.versionLabel.setFont(myFont)
+        self.versionLabel.setStyleSheet("color:#ffffff")
+        self.Titlelayout.addWidget(self.versionLabel)
+        
+        self.billingButton = QtWidgets.QPushButton(self.centralwidget)
+        self.billingButton.setGeometry(QtCore.QRect(80, 670, 181, 61))
+        self.billingButton.setStyleSheet("background-color: rgb(90, 255, 109);\n"
+"font: 500 12pt \"Poppins\";")
+        
+        
+        self.icon = QtGui.QIcon()
+        self.icon.addPixmap(QtGui.QPixmap("ui_elements/startbilling.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.icon4 =  QtGui.QIcon()
+        self.icon4.addPixmap(QtGui.QPixmap("ui_elements/stopbilling.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        
+        self.billingButton.setIcon(self.icon)
+        self.billingButton.setIconSize(QtCore.QSize(27, 27))
+        self.billingButton.setObjectName("billingButton")
+        self.lockButton = QtWidgets.QPushButton(self.centralwidget)
+        self.lockButton.setGeometry(QtCore.QRect(280, 670, 181, 61))
+        self.lockButton.setStyleSheet("background-color: rgb(90, 255, 109);\n"
+"font: 500 12pt \"Poppins\";")
+        
+        icon1 = QtGui.QIcon()
+        icon1.addPixmap(QtGui.QPixmap("ui_elements/lockitems.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.lockButton.setIcon(icon1)
+        self.lockButton.setIconSize(QtCore.QSize(27, 27))
         self.lockButton.setObjectName("lockButton")
-        self.lockButton.clicked.connect(self.lock_clicked)
+        self.resetButton = QtWidgets.QPushButton(self.centralwidget)
+        self.resetButton.setGeometry(QtCore.QRect(480, 670, 181, 61))
+        self.resetButton.setStyleSheet("background-color: rgb(90, 255, 109);\n"
+"font: 500 12pt \"Poppins\";")
         
-        self.labelTotal = QtWidgets.QLabel(Dialog)
-        self.labelTotal.setText("Total: Tk.00")
+        icon2 = QtGui.QIcon()
+        icon2.addPixmap(QtGui.QPixmap("ui_elements/clearbill.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.resetButton.setIcon(icon2)
+        self.resetButton.setIconSize(QtCore.QSize(27, 27))
+        self.resetButton.setObjectName("resetButton")
+        
+        
+        self.horizontalLayoutWidget = QtWidgets.QWidget(self.centralwidget)
+        self.horizontalLayoutWidget.setGeometry(QtCore.QRect(720, 670, 371, 61))
+        self.horizontalLayoutWidget.setObjectName("horizontalLayoutWidget")
+        self.totalLayout = QtWidgets.QHBoxLayout(self.horizontalLayoutWidget)
+        self.totalLayout.setContentsMargins(0, 0, 0, 0)
+        self.totalLayout.setObjectName("totalLayout")
+        
+        
+        
+        self.labelTotal = QtWidgets.QLabel(self.horizontalLayoutWidget)
+        self.labelTotal = QtWidgets.QLabel(self.centralwidget)
+        self.labelTotal.setAlignment(QtCore.Qt.AlignCenter)
+        self.labelTotal.setGeometry(QtCore.QRect(760, 670, 151, 61))
+        self.labelTotal.setText("Total: " +"\u09F3" +"0")
         self.labelTotal.setFont(myFont)
-        self.labelTotal.setGeometry(720, 550, 130, 20)
+        self.labelTotal.setStyleSheet("color:#ffffff; border: 2px solid white;")
+        self.labelTotal.setObjectName("labelTotal")
+        self.totalLayout.addWidget(self.labelTotal)
+        
+        
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 1144, 21))
+        
+       
+       
+        self.menubar.setPalette(palette)
+        self.menubar.setStyleSheet("color:#ffffff;")
+        self.menubar.setNativeMenuBar(True)
+        self.menubar.setObjectName("menubar")
+        self.menuFIle = QtWidgets.QMenu(self.menubar)
+        self.menuFIle.setObjectName("menuFIle")
+        self.menuFIle.setStyleSheet("background-color:#3b3e3f")
+        
+        self.menuAction = QtWidgets.QMenu(self.menubar)
+        self.menuAction.setObjectName("menuAction")
+        self.menuAction.setStyleSheet("background-color:#3b3e3f")
+        
+        self.menuHelp = QtWidgets.QMenu(self.menubar)
+        self.menuHelp.setObjectName("menuHelp")
+        self.menuHelp.setStyleSheet("background-color:#3b3e3f")
+        MainWindow.setMenuBar(self.menubar)
+        
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+        
+        self.actionOpen_Price_File = QtWidgets.QAction(MainWindow)
+        self.actionOpen_Price_File.setObjectName("actionOpen_Price_File")
+        
+        self.actionExit = QtWidgets.QAction(MainWindow)
+        self.actionExit.setObjectName("actionExit")
+        
+        self.actionStart_Billing = QtWidgets.QAction(MainWindow)
+        self.actionStart_Billing.setObjectName("actionStart_Billing")
+        
+        self.actionStop_Billing = QtWidgets.QAction(MainWindow)
+        self.actionStop_Billing.setObjectName("actionStop_Billing")
+        
+        self.actionLock_Items = QtWidgets.QAction(MainWindow)
+        self.actionLock_Items.setObjectName("actionLock_Items")
+        
+        self.actionClear_List = QtWidgets.QAction(MainWindow)
+        self.actionClear_List.setObjectName("actionClear_List")
+        
+        self.actionHow_to_use = QtWidgets.QAction(MainWindow)
+        self.actionHow_to_use.setObjectName("actionHow_to_use")
+        
+        self.actionAbout = QtWidgets.QAction(MainWindow)
+        self.actionAbout.setObjectName("actionAbout")
+        
+        self.menuFIle.addAction(self.actionOpen_Price_File)
+        self.menuFIle.addAction(self.actionExit)
+        
+        self.menuAction.addAction(self.actionStart_Billing)
+        self.menuAction.addAction(self.actionStop_Billing)
+        self.menuAction.addAction(self.actionLock_Items)
+        self.menuAction.addAction(self.actionClear_List)
+        
+        self.menuHelp.addAction(self.actionHow_to_use)
+        self.menuHelp.addAction(self.actionAbout)
+        
+        self.menubar.addAction(self.menuFIle.menuAction())
+        self.menubar.addAction(self.menuAction.menuAction())
+        self.menubar.addAction(self.menuHelp.menuAction())
+        
+        self.lockButton.clicked.connect(self.lock_clicked)
+        self.billingButton.clicked.connect(self.button_clicked)
+        self.resetButton.clicked.connect(self.reset_list)
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
-        self.retranslateUi(Dialog)
-        QtCore.QMetaObject.connectSlotsByName(Dialog)
-
-    def retranslateUi(self, Dialog):
+    def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        Dialog.setWindowTitle(_translate("Automated Checkout System", "Automated Checkout System"))
-        self.pushButton.setText(_translate("Automated Checkout System", "Start Billing"))
-        self.lockButton.setText(_translate("Automated Checkout System", "Lock Current Items"))
-        self.resetButton.setText(_translate("Automated Checkout System", "Clear Bill"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "Automated Billing System v.1.0"))
+        self.billingButton.setText(_translate("MainWindow", " START BILLING"))
+        self.lockButton.setText(_translate("MainWindow", "  LOCK ITEMS"))
+        self.resetButton.setText(_translate("MainWindow", "  CLEAR LIST"))
+        #self.labelTotal.setText(_translate("MainWindow", "<html><head/><body><p><span style=\" font-family:\'poppins\'; font-size:16pt; font-weight:600; color:#ffffff;\">Total: </span></p></body></html>"))
+        self.menuFIle.setTitle(_translate("MainWindow", "FIle"))
+        self.menuAction.setTitle(_translate("MainWindow", "Action"))
+        self.menuHelp.setTitle(_translate("MainWindow", "Help"))
+        self.actionOpen_Price_File.setText(_translate("MainWindow", "Open Price File"))
+        self.actionExit.setText(_translate("MainWindow", "Exit"))
+        self.actionStart_Billing.setText(_translate("MainWindow", "Start Billing"))
+        self.actionStart_Billing.setShortcut(_translate("MainWindow", "S"))
+        self.actionStop_Billing.setText(_translate("MainWindow", "Stop Billing"))
+        self.actionLock_Items.setText(_translate("MainWindow", "Lock Items"))
+        self.actionLock_Items.setShortcut(_translate("MainWindow", "Space"))
+        self.actionClear_List.setText(_translate("MainWindow", "Clear List"))
+        self.actionClear_List.setShortcut(_translate("MainWindow", "X"))
+        self.actionHow_to_use.setText(_translate("MainWindow", "How to use"))
+        self.actionAbout.setText(_translate("MainWindow", "About"))
+
         
     def update_image(self, cv_img):
         qtimg = self.convert_cv_qt(cv_img)
-        self.label.setPixmap(qtimg)
+        self.camFeed.setPixmap(qtimg)
         
     def convert_cv_qt(self, cv_img):
         """Convert from an opencv image to QPixmap"""
@@ -336,7 +518,7 @@ class Ui_Dialog(object):
             self.listWidget.takeItem(row)
             self.listPrice.takeItem(row)
         
-            
+    #clear list button actually resets list       
     def reset_list(self):
         self.listPrice.clear()
         self.listWidget.clear()
@@ -356,18 +538,28 @@ class Ui_Dialog(object):
         
         self.labelTotal.setText("Total: \u09F3" + str(total))
         self.labelTotal.adjustSize()
+        self.colorize(Qt.black)
+       
+        
         
     def button_clicked(self):
         self.button_flag = not self.button_flag
+        _translate = QtCore.QCoreApplication.translate
         if self.button_flag:
-            self.pushButton.setText("Stop Billing")
+            self.billingButton.setIcon(self.icon4)
+            self.billingButton.setText(_translate("MainWindow", " STOP BILLING"))
             self.colorize(Qt.black)
+            self.billingButton.setStyleSheet("background-color: rgb(255, 92, 92);\n"
+"font: 500 12pt \"Poppins\";")
             
         else:
-            self.pushButton.setText("Start Billing")
+            self.billingButton.setIcon(self.icon)
+            self.billingButton.setText(_translate("MainWindow", " START BILLING"))
             self.clear_list()
             self.update_total()
             self.colorize(Qt.darkGreen)
+            self.billingButton.setStyleSheet("background-color: rgb(90, 255, 109);\n"
+"font: 500 12pt \"Poppins\";")
             
             
     def lock_clicked(self):
@@ -379,7 +571,11 @@ class Ui_Dialog(object):
         print(self.lock_pointer)
         for i in range(self.lock_pointer - 1):
            x = self.listWidget.item(i)
-           x.setBackground(QColor("#7fc97f"))   
+           y = self.listPrice.item(i)
+           x.setBackground(QColor("#7fc97f"))
+           y.setBackground(QColor("#7fc97f"))
+           x.setForeground(QColor("#252728"))
+           y.setForeground(QColor("#252728"))
            
     def colorize(self, color):
         color_effect = QGraphicsColorizeEffect()
@@ -394,10 +590,10 @@ class Ui_Dialog(object):
 if __name__ == "__main__":
     
     app = QtWidgets.QApplication(sys.argv)
-    Dialog = QtWidgets.QDialog()
-    ui = Ui_Dialog()
-    ui.setupUi(Dialog)
-    Dialog.show()
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
     
     run()
     
